c3c37c657b6dd554e4bce33124c40c3c05aec338
Edward23-ux <edwardcampos46@gmail.com>
2026-07-14 00:50:09 -0500

    Se corrigió el bug de hora

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1143,7 +1143,7 @@ export default function DashboardPage() {
                               fontSize: '13px',
                               whiteSpace: 'nowrap',
                             }}>
-                              {formatFecha(t.fecha)}
+                              {hora}
                             </td>
                           </tr>
                         );
